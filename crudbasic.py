906f15d6cb786efcf7c213a20501bc69842ed1a4
Holanda03 <carlosabraao@hotmail.com.br>
2023-05-19 11:37:44 -0300

    adicionando metodo addProduto

--- crudbasic.py
+++ crudbasic.py
@@ -10,9 +10,21 @@ db = mysql.connector.connect (
 c = db.cursor()
 
 # Connection test
-c.execute("SELECT CURDATE()")
-row = c.fetchone()
-print("Current Date: {0}".format(row[0]))
+#c.execute("SELECT CURDATE()")
+#row = c.fetchone()
+#print("Current Date: {0}".format(row[0]))
+
+
+#CREATE
+def addProduto(nome, quantidade, valor):
+    sql = f'INSERT INTO produto (nome, quantidade, valor) VALUES ("{nome}", {quantidade}, {valor})'
+    c.execute(sql)
+    db.commit()
+
+
+# Tests
+#addProduto("panela", 2, 35)
+#addProduto("tampa", 2, 7.50)
 
 
 c.close()
